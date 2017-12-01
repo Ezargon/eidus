2ff6fa12f4bd5ab5ef59731bf5068d3a5431c233
rtavero <rtavero@us.es>
2017-12-01 13:13:32 +0100

    Cambio botón de "Ver Calendario Completo"

---
+++ logs/jcontroller.log.php
@@ -0,0 +1,22 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-11-27 09:14:13 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-11-27T09:14:13+00:00	INFO ::1	controller	Holding edit ID com_modules.edit.module.222 Array (     [0] => 222 ) 
+2017-11-27T09:14:13+00:00	INFO ::1	controller	Checking edit ID com_modules.edit.module.222: 1 Array (     [0] => 222 ) 
+2017-11-27T09:16:00+00:00	INFO ::1	controller	Holding edit ID com_modules.edit.module.222 Array (     [0] => 222 ) 
+2017-11-27T09:16:00+00:00	INFO ::1	controller	Checking edit ID com_modules.edit.module.222: 1 Array (     [0] => 222 ) 
+2017-11-27T09:16:53+00:00	INFO ::1	controller	Checking edit ID com_modules.edit.module.222: 1 Array (     [0] => 222 ) 
+2017-11-27T09:17:00+00:00	INFO ::1	controller	Releasing edit ID com_modules.edit.module.222 Array ( ) 
+2017-11-27T09:21:07+00:00	INFO ::1	controller	Holding edit ID com_modules.edit.module.222 Array (     [0] => 222 ) 
+2017-11-27T09:21:07+00:00	INFO ::1	controller	Checking edit ID com_modules.edit.module.222: 1 Array (     [0] => 222 ) 
+2017-11-29T10:18:48+00:00	INFO ::1	controller	Holding edit ID com_modules.edit.module.222 Array (     [0] => 222 ) 
+2017-11-29T10:18:48+00:00	INFO ::1	controller	Checking edit ID com_modules.edit.module.222: 1 Array (     [0] => 222 ) 
+2017-11-29T10:19:59+00:00	INFO ::1	controller	Holding edit ID com_modules.edit.module.222 Array (     [0] => 222 ) 
+2017-11-29T10:20:00+00:00	INFO ::1	controller	Checking edit ID com_modules.edit.module.222: 1 Array (     [0] => 222 ) 
+2017-11-29T10:20:31+00:00	INFO ::1	controller	Holding edit ID com_modules.edit.module.222 Array (     [0] => 222 ) 
+2017-11-29T10:20:31+00:00	INFO ::1	controller	Checking edit ID com_modules.edit.module.222: 1 Array (     [0] => 222 ) 
+2017-11-29T10:21:06+00:00	INFO ::1	controller	Holding edit ID com_modules.edit.module.222 Array (     [0] => 222 ) 
+2017-11-29T10:21:06+00:00	INFO ::1	controller	Checking edit ID com_modules.edit.module.222: 1 Array (     [0] => 222 ) 
